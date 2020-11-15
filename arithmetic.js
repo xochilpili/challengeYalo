64c1d58b2a1b70bd976683ffe62f90be14da1434
xochilpili <xochilpili@gmail.com>
2020-11-14 20:55:21 -0600

    Adding more exercises

--- arithmetic.js
+++ arithmetic.js
@@ -76,10 +76,43 @@ const test_three = {
 		error: 50
 	}
 };
+
+const test_four = {
+	expression: '12 + (5**2) - 3',
+	save: 'result',
+	transitions:{
+		next: 101,
+		error: 102
+	}
+};
+
+const test_five = {
+	expression: '(3 + 2) * (5 - 2) / (3 + 3)',
+	save: 'result',
+	transitions: {
+		next: 101,
+		error: 102
+	}
+};
+
+const test_six = {
+	expression: '(3+2) + 5!',
+	save: 'result',
+	transitions:{
+		next: 101,
+		error: 102
+	}
+}
+
 console.log(`----- > Example 1 : ${test_one.expression} ----`);
 arithmeticChallenge(JSON.stringify(test_one));
 console.log(`----- > Example 2 : ${test_two.expression} ----`);
 arithmeticChallenge(JSON.stringify(test_two));
 console.log(`----- > Example 3 : ${test_three.expression} ----`);
 arithmeticChallenge(JSON.stringify(test_three));
-
+console.log(`----- > Example 4 : ${test_four.expression} ----`);
+arithmeticChallenge(JSON.stringify(test_four));
+console.log(`----- > Example 5 : ${test_five.expression} ----`);
+arithmeticChallenge(JSON.stringify(test_five));
+console.log(`----- > Example 6 : ${test_six.expression} ----`);
+arithmeticChallenge(JSON.stringify(test_six));
